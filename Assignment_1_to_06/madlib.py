fec25d6bc2307d142d2e7b6ec79d7e818161d9f3
ERUM WARIS <159144847+erum-waris@users.noreply.github.com>
2025-04-21 15:16:34 +0500

    madlib updated

---
+++ Assignment_1_to_06/madlib.py
@@ -0,0 +1,8 @@
+name = "Erum Waris"
+age = 31
+hobby = "Coding"
+favorite_color = "white and black"
+favorite_food = "Pizza"
+
+sentence = f"My name is {name}, I am {age} years old, my hobby is {hobby}, my favorite color is {favorite_color}, and my favorite food is {favorite_food}."
+print(sentence) # Print the formatted sentence using f string
